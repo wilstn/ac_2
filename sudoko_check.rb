56df596c4b287672b10371655764aead6a707a62
wilson <wt@wtcom.local>
2017-01-31 23:15:52 +0800

    Change sudoko check to check for value 15

--- sudoko_check.rb
+++ sudoko_check.rb
@@ -3,29 +3,34 @@ def read_file(url)
   File.open(url).each_line do |f|
     file_data << f.strip
   end
+  file_data.each_with_index do |row, idx|
+    file_data[idx] = row.split(',').map { |each_s| each_s.to_i }
+  end
   return file_data
 end
 
-file_items = read_file("testing_sudoku.txt")
+# print read_file("testing_sudoku.txt"), "\n"
 
-new_array = []
+contents = read_file("testing_sudoku.txt")
+puts "Your grid is:"
+contents.each {|each_row| puts each_row.join(',')}
 
-file_items.each do |element|
-  new_array << element.split(',')
+is_good = true
+
+contents.each_with_index do |row_check, idx_row|
+  col_values = contents[0][idx_row] + contents[1][idx_row] + contents[2][idx_row]
+  if row_check.reduce(:+) == 15
+    next
+  else
+    is_good = false
+    puts "Row #{idx_row + 1}'s values: #{row_check} do not add up to 15"
+  end
+  if col_values == 15
+    next
+  else
+    is_good = false
+    puts "Column #{idx_row + 1}'s values #{contents[0][idx_row]}, #{contents[1][idx_row]}, #{contents[2][idx_row]} do not add up to 15"
+  end
 end
 
-new_array.each do |value|
-  new_array2 << value.to_i
-end
-
-new_array2 = new_array.flatten
-summation = nil
-
-
-print new_array.flatten.sort
-#
-# print new_array
-# def constraints(file)
-#   file.each {|num| sum+=num}
-#     if sum > 45
-#       print ""
+puts "The grid seems good" if is_good
