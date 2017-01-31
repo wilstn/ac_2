f3f91a5dfbde74975dab17c6dab3dbbe4babb48e
wilson <wt@wtcom.local>
2017-01-31 23:40:54 +0800

    Try to make it readable

--- sudoko_check.rb
+++ sudoko_check.rb
@@ -9,8 +9,6 @@ def read_file(url)
   return file_data
 end
 
-# print read_file("testing_sudoku.txt"), "\n"
-
 contents = read_file("testing_sudoku.txt")
 puts "Your grid is:"
 contents.each {|each_row| puts each_row.join(',')}
@@ -19,17 +17,17 @@ is_good = true
 
 contents.each_with_index do |row_check, idx_row|
   col_values = contents[0][idx_row] + contents[1][idx_row] + contents[2][idx_row]
-  if row_check.reduce(:+) == 15
 
-  else
+  if row_check.reduce(:+) != 15
     is_good = false
     puts "Row #{idx_row + 1}'s values: #{row_check} do not add up to 15"
-  end
-  if col_values == 15
-    
   else
+  end
+
+  if col_values != 15
     is_good = false
     puts "Column #{idx_row + 1}'s values #{contents[0][idx_row]}, #{contents[1][idx_row]}, #{contents[2][idx_row]} do not add up to 15"
+  else
   end
 end
 
